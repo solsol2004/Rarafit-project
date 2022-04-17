f89b60fc34ddde0f351d17f4cb1c8f86ef6c5a85
Stella <dpthf24@gmail.com>
2022-04-18 08:13:39 +0900

    update

---
+++ assets/js/schedule.js
@@ -0,0 +1,40 @@
+$(".mon").click(function () {
+  $(".mon-table").removeClass("hidden");
+  $(".tue-table,.wed-table,.thu-table,.fri-table,.sat-table").addClass(
+    "hidden"
+  );
+});
+$(".tue").click(function () {
+  $(".tue-table").removeClass("hidden");
+  $(".mon-table,.wed-table,.thu-table,.fri-table,.sat-table").addClass(
+    "hidden"
+  );
+});
+
+$(".wed").click(function () {
+  $(".wed-table").removeClass("hidden");
+  $(".mon-table,.tue-table,.thu-table,.fri-table,.sat-table").addClass(
+    "hidden"
+  );
+});
+
+$(".thu").click(function () {
+  $(".thu-table").removeClass("hidden");
+  $(".tue-table,.wed-table,.mon-table,.fri-table,.sat-table").addClass(
+    "hidden"
+  );
+});
+
+$(".fri").click(function () {
+  $(".fri-table").removeClass("hidden");
+  $(".tue-table,.wed-table,.thu-table,.mon-table,.sat-table").addClass(
+    "hidden"
+  );
+});
+
+$(".sat").click(function () {
+  $(".sat-table").removeClass("hidden");
+  $(".tue-table,.wed-table,.thu-table,.fri-table,.mon-table").addClass(
+    "hidden"
+  );
+});
