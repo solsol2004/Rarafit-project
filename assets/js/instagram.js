0239a3ae22a9b4eecb51557afbb381ec665aac00
Stella <dpthf24@gmail.com>
2022-04-20 23:44:22 +0900

    Update instagram.js

--- assets/js/instagram.js
+++ assets/js/instagram.js
@@ -6,6 +6,7 @@ var feed = new Instafeed({
   sortBy: "most-recent",
   template:
     '<div class="col-lg-3 col-md-3 col-sm-4 col-xs-4 instaimg"><a href="{{link}}" title="{{caption}}" target="_blank"><img src="{{image}}" class="img-fluid"/></a></div>',
-  accessToken: "여기에 토큰 입력",
+  accessToken:
+    "IGQVJXM240QXNCZAnRLaklTbXFTQWY0UjZAPaHpJVnR4WWZAwc2NtTVBmVUZAXY192Q1lydXBERWZAHTHplT0FZAWVJicTJHMjU4OUkxLW5vc0xRWlhWLTRiTFl0UVdObkswTzlZARDFSdjU3YlFBQkJTMGpwegZDZD",
 });
 feed.run();
