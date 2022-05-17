0197983fe9e323ab01e87948de62b627c50d097c
Stella <77248703+solsol2004@users.noreply.github.com>
2022-05-17 17:06:03 +0900

    update

---
+++ admin/js/resume-detail.js
@@ -0,0 +1,17 @@
+const db = firebase.firestore();
+const storage = firebase.storage();
+
+var singleQuestion = new URLSearchParams(window.location.search);
+
+db.collection("job")
+  .doc(singleQuestion.get("id"))
+  .get()
+  .then((result) => {
+    console.log(result.data());
+    $("#resume-name").html(result.data().성함);
+    $("#resume-birthday").html(result.data().생년월일);
+    $("#resume-school").html(result.data().졸업학교);
+    $("#resume-field").html(result.data().희망부문);
+    $("#resume-contact").html(result.data().연락처);
+    $("#resume-description").html(result.data().자기소개);
+  });
